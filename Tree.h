94cfce079b3b0649cd034cca3af9daf5fbd4e8f8
Ankur Gupta <gm.ankurgarg@gmail.com>
2013-06-16 03:20:46 +0530

    Update Tree.h

--- Tree.h
+++ Tree.h
@@ -1,1 +1,20 @@
-#
+/**
+This is the header file containing the declaration of primary functions related to Binary Tree
+
+@name   Tree.h
+**/
+
+struct Node
+{
+  int data;
+  Node *left;
+  Node *right;
+};
+
+
+Node *  BuildBinaryTree(int *arr,int p,int q);
+int     Check(Node *node, int m, int c);
+void    Read(Node *node);
+int     BTIterativeHeight(Node *root);
+int     BTRecursiveHeight(Node *node);
+Node *  Search(Node *root,int i);
